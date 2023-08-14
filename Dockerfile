7d42a095d84ff213e4e8a4072c9ff21ff55fc5e5
“nigromante” <julianvidal@live.cl>
2023-08-14 02:05:27 -0400

    REDIS Session

--- Dockerfile
+++ Dockerfile
@@ -14,8 +14,8 @@ RUN go get github.com/mailhog/mhsendmail
 RUN cp /root/go/bin/mhsendmail /usr/bin/mhsendmail
 RUN echo 'sendmail_path = /usr/bin/mhsendmail --smtp-addr mailhog:1025' >> /usr/local/etc/php/php.ini
 
-# RUN echo 'session.save_handler = redis' >> /usr/local/etc/php/php.ini
-# RUN echo 'session.save_path = tcp://redis:6379' >> /usr/local/etc/php/php.ini
+RUN echo 'session.save_handler = redis' >> /usr/local/etc/php/php.ini
+RUN echo 'session.save_path = tcp://redis:6379' >> /usr/local/etc/php/php.ini
 
 RUN a2enmod rewrite
 RUN a2enmod ssl
--- src/framework/Session.php
+++ src/framework/Session.php
@@ -1,16 +1,16 @@
 <?php
 
-use Framework\SessionManager;
+// use Framework\SessionManager;
 
-$handler_session = new SessionManager( $config['session'] , $ClientIpAddress );
-session_set_save_handler(
-    [$handler_session, 'open'],
-    [$handler_session, 'close'],
-    [$handler_session, 'read'],
-    [$handler_session, 'write'],
-    [$handler_session, 'destroy'],
-    [$handler_session, 'gc']
-);
+// $handler_session = new SessionManager( $config['session'] , $ClientIpAddress );
+// session_set_save_handler(
+//     [$handler_session, 'open'],
+//     [$handler_session, 'close'],
+//     [$handler_session, 'read'],
+//     [$handler_session, 'write'],
+//     [$handler_session, 'destroy'],
+//     [$handler_session, 'gc']
+// );
 
 
 session_name($config['session']['SESSION_KEY']);
