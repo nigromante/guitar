bde2e40ec79e67bc2b8929195d3850906580fdaf
“nigromante” <julianvidal@live.cl>
2023-08-25 17:09:40 -0400

    aislar rabbit mq

--- src/config/routes/api.php
+++ src/config/routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
 use Controllers\api\ApiHelpController;
-use Controllers\api\RabbitController;
 
 use Framework\Router;
 
-
 Router::Get('/api', [ApiHelpController::class, 'index']);
 
-Router::Get('/api/rabbit/send', [RabbitController::class, 'send']);
