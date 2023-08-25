f5ce29e0fec412633a32d39324542a3bdf00c358
“nigromante” <julianvidal@live.cl>
2023-08-25 19:38:17 -0400

    extraer workers rabbi

--- src/Workers/HelloProducerCallTest.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-require_once  '../vendor/autoload.php';
-system('clear');
-
-use Workers\HelloProducerCall;
-
-HelloProducerCall::Send( "julianvidal@live.cl" ) ;
--- src/Workers/RabbitMQ/RabbitMQConnectionCommand.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Workers\RabbitMQ;
-
-class RabbitMQConnectionCommand {
-
-    public function __construct( public string $server , public int $port, public string $user, public string $password ) {}
-
-}
