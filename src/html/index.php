b164fadaee5d5ed47871d29f616617eb945452c1
“nigromante” <julianvidal@live.cl>
2023-08-28 00:09:46 -0400

    clean index

--- src/html/index.php
+++ src/html/index.php
@@ -2,21 +2,13 @@
 require_once "../vendor/autoload.php";
 
 date_default_timezone_set('America/Santiago');
-
 require_once "../config/load.php";
-
-
 require_once "../config/routes.php";
-
 require_once "../utilities/framework_wrappers.php";
 
 
-use Nigromante\Framework\Application;
+Nigromante\Framework\Session::Start( $config ) ;
 
-use Nigromante\Framework\Session as SessionFinal;
-
-SessionFinal::Start( $config ) ;
-
-(new Application( $config ))
+(new  Nigromante\Framework\Application( $config ))
     ->run() ; 
 
